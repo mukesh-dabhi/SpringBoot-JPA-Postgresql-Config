a5663130c8411e5dfa11ce4d3fbe74404335625f
mukesh-technoark <56184391+mukesh-technoark@users.noreply.github.com>
2019-11-27 12:01:59 +0530

    Initial Commit

---
+++ src/main/java/com/mukesh/jpa/postgresql/SpringBootJpaPostgresqlConfigApplication.java
@@ -0,0 +1,13 @@
+package com.mukesh.jpa.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaPostgresqlConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaPostgresqlConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mukesh/jpa/postgresql/SpringBootJpaPostgresqlConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukesh.jpa.postgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaPostgresqlConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
